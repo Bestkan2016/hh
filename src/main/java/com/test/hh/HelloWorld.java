6c00e9aaa1d2a52d2013f98ba93445234802cb97
admin <admin@qq.com>
2018-08-17 18:13:51 +0800

    jdk10

---
+++ src/main/java/com/test/hh/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.test.hh;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class HelloWorld {
+
+
+
+    public static void main(String[] args) {
+
+        var s2 = List.of("Hello", "World");
+
+        var s3 = new ArrayList<>();
+        s3.add("1");
+        s3.add(2);
+        s3.add(false);
+        s3.add(3.14);
+
+        s3.stream().filter(o -> o.equals("1")).forEach(o -> System.out.println(o));
+    }
+
+}
